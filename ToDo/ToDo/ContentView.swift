d460b5ce144cae9db65d3efac5b3b2f6e5a5d6c1
qiuzhongxin <397260568@qq.com>
2024-10-23 01:30:50 +0800

    创建方法toggleToDoItemCompleted

--- ToDo/ToDo/ContentView.swift
+++ ToDo/ToDo/ContentView.swift
@@ -1,21 +1,46 @@
-//
-//  ContentView.swift
-//  ToDo
-//
-//  Created by Zhongxin qiu on 2024/10/22.
-//
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State var todoItems: [ToDoModel] = [
+        ToDoModel(todoItem: "阅读1小时"),
+        ToDoModel(todoItem: "跑步5公里")
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            ForEach(todoItems) { todoItem in
+                ToDoListRow(todoItem: todoItem)
+                    .onTapGesture {
+                            toggleToDoItemCompleted(todoItem)
+                        }
+            }
+            .listRowSeparator(.hidden)
         }
-        .padding()
+        .listStyle(PlainListStyle())
+    }
+    //点击完成事项
+    func toggleToDoItemCompleted(_ todoItem: ToDoModel) {
+        if let index = todoItems.firstIndex(where: {$0.id == todoItem.id} ) {
+            todoItems[index].isCompleted.toggle()
+        }
+    }
+}
+struct ToDoListRow: View {
+    var todoItem: ToDoModel
+    var body: some View {
+        HStack{
+            Text(todoItem.todoItem)
+                .foregroundColor(todoItem.isCompleted ? .gray : .black)
+                .lineLimit(1)
+                .strikethrough(todoItem.isCompleted, color: .gray)//删除线
+            Spacer()
+            Image(systemName: todoItem.isCompleted ? "checkmark.circle.fill" : "circle")
+                .foregroundColor(todoItem.isCompleted ? .green : .black)
+        }
+        .padding(.horizontal)
+        .frame(height: 75)
+        .background(Color(.systemGray6))
     }
 }
 
---
+++ ToDo/ToDo/ToDoModel.swift
@@ -0,0 +1,13 @@
+
+import SwiftUI
+
+struct ToDoModel: Identifiable {
+    var id: UUID = UUID()
+    var todoItem: String
+    var isCompleted: Bool
+    
+    init(todoItem: String) {
+        self.todoItem = todoItem
+        isCompleted = false
+    }
+}
