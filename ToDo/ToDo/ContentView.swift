b58d6a3fd642bc76572f10dd5718cc117fffed3f
qiuzhongxin <397260568@qq.com>
2024-10-23 22:14:30 +0800

    ToDo视图新增删除功能

--- ToDo/ToDo/ContentView.swift
+++ ToDo/ToDo/ContentView.swift
@@ -15,16 +15,23 @@ struct ContentView: View {
                             toggleToDoItemCompleted(todoItem)
                         }
             }
+            //滑动删除
+            .onDelete(perform: deleteToDoItem)
             .listRowSeparator(.hidden)
         }
         .listStyle(PlainListStyle())
-        InputTextField()
+        
+        InputTextField(todoItems: $todoItems)
     }
     //点击完成事项
     func toggleToDoItemCompleted(_ todoItem: ToDoModel) {
         if let index = todoItems.firstIndex(where: {$0.id == todoItem.id} ) {
             todoItems[index].isCompleted.toggle()
         }
+    }
+    //删除事项方法
+    func deleteToDoItem(at offsets: IndexSet) {
+        todoItems.remove(atOffsets: offsets)
     }
 }
 struct ToDoListRow: View {
@@ -46,6 +53,7 @@ struct ToDoListRow: View {
 }
 struct InputTextField: View {
     @State var newToDoItem = ""
+    @Binding var todoItems: [ToDoModel]
     var body: some View {
         HStack{
             TextField("添加新事项", text: $newToDoItem)
@@ -53,8 +61,18 @@ struct InputTextField: View {
             Image(systemName: "plus.circle.fill")
                 .font(.system(size: 28))
                 .foregroundColor(.blue)
+                .onTapGesture {
+                    addToDoItem()
+                }
         }
         .padding()
+    }
+    //新增笔记的方法
+    func addToDoItem() {
+        if !newToDoItem.isEmpty {
+            todoItems.append(ToDoModel(todoItem: newToDoItem))
+            newToDoItem = ""
+        }
     }
 }
 
